414da53e625c8830aaa9fefd68412d1063f6a567
emme <emmeran.bordessoules@gmail.com>
2015-05-29 21:40:55 +0200

    git

--- projet_web/settings.py
+++ projet_web/settings.py
@@ -63,16 +63,7 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
 
-
 DEBUG = True
-
-
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
 
 DATABASES = {
 	'default': {
