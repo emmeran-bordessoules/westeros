9d963aa43ff6ae74ce0e4778f34b96491552f9d2
emme <emmeran.bordessoules@gmail.com>
2015-05-30 19:28:07 +0200

    yolo

--- carto/views.py
+++ carto/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from vote.models import scoreDep
+from vote.models import scoreDep,Vote
 
 def home(request):
-	# score=scoreDep.objects.all()
-	# for i in score: {'sd':sd}
-		# sd+=scoreDep.objects.filter(=NumDep,i=VoteDep).max(ScoreVote)
-	return render(request,'carto/carto.html',locals())
+	score=scoreDep.objects.order_by('NumDep').order_by('-ScoreDep')[:8:1]
+	return render(request,'carto/carto.html',{'score':score})
+
+
+	
+	
+	
--- projet_web/urls.py
+++ projet_web/urls.py
@@ -12,5 +12,3 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^/',include('vote.urls')),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# if settings.DEBUG:
-	# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
