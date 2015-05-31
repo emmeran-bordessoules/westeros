abb38f90b02ca9b643092d45bdc8359c6ff2b769
emme <emmeran.bordessoules@gmail.com>
2015-05-31 09:57:03 +0200

    gg

--- carto/views.py
+++ carto/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from vote.models import scoreDep
 
 def home(request):
-	score=scoreDep.objects.order_by('NumDep').order_by('-ScoreDep')[:100:1]
+	score=scoreDep.objects.order_by('-ScoreDep','NumDep')[:100:1]
 	return render(request,'carto/carto.html',{'score':score})
 
