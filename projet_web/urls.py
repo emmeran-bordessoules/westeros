c3839fede98433b287b0e63893c48642009ab04b
emmeran-bordessoules <emmeran.bordessoules@gmail.com>
2015-06-02 14:46:36 +0200

    Update urls.py

--- projet_web/urls.py
+++ projet_web/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 	url(r'^comment/',include('comment.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^/',include('vote.urls')),
-	url(r'^$','vote.views.accueil'),
+#	url(r'^$','vote.views.accueil'),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
